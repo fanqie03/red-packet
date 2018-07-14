45f289e513358268b977f149bd8ce9d1a349717f
mfchen <mfchen1996@hotmail.com>
2018-07-14 14:43:04 +0800

    delete file

--- src/cn/edu/scau/cmi/chenmengfu/redpacket/Client.java
+++ src/cn/edu/scau/cmi/chenmengfu/redpacket/Client.java
@@ -1,10 +1,11 @@
 package cn.edu.scau.cmi.chenmengfu.redpacket;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BeansException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Client client = (Client) context.getBean("client");
 		System.out.println(client);
